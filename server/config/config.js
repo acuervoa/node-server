01c8c7bb29805b30f85a3233143b104b163f1e76
acuervoa <kuerbo@gmail.com>
2018-12-26 22:20:40 +0100

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,14 @@
 process.env.PORT = process.env.PORT || 3000;
+
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://username:1secret@ds129904.mlab.com:29904/nodeserverrest'
+}
+
+process.env.URLDB = urlDB;
